e20d37184408c0e721327bb9576ac36e3728db30
Ana Karen Guerrero Geronimo <akg45@njit.edu>
2024-11-14 19:47:04 -0500

    lab8

---
+++ lab8.js
@@ -0,0 +1,81 @@
+let username="Ana";
+
+let userage=21;
+
+console.log(username)
+console.log(userage)
+
+
+//Question 2
+
+let studentname="Ana"
+
+let studentage=21
+
+let grade=true
+
+let year; 2024
+
+console.log(studentname)
+console.log(studentage)
+console.log(grade)
+console.log(year)
+
+//Question 3
+
+let rollnumber=15 
+if (rollnumber>10){
+console.log("The RollNumber is greater than 10"); 
+}
+//Question 4
+let ismember=true
+console.log("Member discount applied");
+//Question 5
+let startvalue=50;
+if(startvalue>0){
+console.log("startvalue is positive");
+}else if(startvalue<0) {
+console.log("startvalue is negative");
+}else{(startvalue=0)
+console.log("startvalue is zero");
+}
+//Question 6
+let a=10;
+let b=20;
+console.log("a == b:", a== b);
+console.log("a=== b", a===b);
+console.log("a!= b", a!=b);
+console.log("a> b", a> b)
+console.log("a< b", a< b)
+console.log("a>=b", a>=b)
+console.log("a<=b", a<= b)
+//Question 7
+let firstname="Ana";
+let lastname="Guerrero";
+let age="21";
+console.log("Hello, Ana Guerrero, you are 21 years old");
+//Question 8
+let num1=50;
+if(num1>0 && num1<100){
+    console.log("The number is within range");}
+//Question 9
+let hasCar=true;
+let hasLicense=true;
+let hasInsurance=true;
+if (hasCar && hasLicense && hasInsurance){
+    console.log("You can drive legally");}
+    else{console.log("Check your driving elegibility")}
+//Question 10
+let num2 = 21;
+if (num2 % 2===0) {
+console.log("The number is even");
+} else{console.log("the number is odd");}
+//Question 11
+let score = 85;
+if (score>90){
+    console.log("Grade A");}
+    else if (score >= 80 && score<= 89){
+    console.log("Grade B");
+} else{
+    console.log("Grade C");
+}
